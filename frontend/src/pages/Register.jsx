7366231524d54d2622f76ee1afcbb37df42d400c
jonokorn <jonasosterkorn@googlemail.com>
2022-04-04 23:18:17 +0200

    Added register forms

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,81 @@
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = () => {
+
+  }
   return (
-    <div>Register</div>
+    <section className='heading'>
+      <h1>Register</h1>
+      <section className='form'>
+        <form>
+
+          {/* Input for Username */}
+          <div className="form-group">
+            <input 
+            type="text"
+            className="form-control"
+            id='name' 
+            name='name'
+            value={name}
+            placeholder='name'
+            onChange={onChange}
+            />
+          </div>
+
+          {/* Input for User Email */}
+          <div className="form-group">
+            <input 
+            type="text"
+            className="form-control"
+            id='email' 
+            name='email'
+            value={email}
+            placeholder='email'
+            onChange={onChange}
+            />
+          </div>
+
+          {/* Input for User Password */}
+          <div className="form-group">
+            <input 
+            type="password"
+            className="form-control"
+            id='password' 
+            name='password'
+            value={password}
+            placeholder='password'
+            onChange={onChange}
+            />
+          </div>
+
+          {/* Input for User Password Confirm */}
+          <div className="form-group">
+            <input 
+            type="password"
+            className="form-control"
+            id='password2' 
+            name='password2'
+            value={password2}
+            placeholder='confirm password'
+            onChange={onChange}
+            />
+          </div>
+
+        </form>
+      </section>
+
+    </section>
   )
 }
 
